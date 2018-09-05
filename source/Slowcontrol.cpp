258a08851d18f32d79387dc3b386730d52873626
flagarde <lagarde@sjtu.edu.cn>
2018-09-05 18:41:21 +0800

    Clean-up

--- source/Slowcontrol.cpp
+++ source/Slowcontrol.cpp
@@ -7,7 +7,6 @@
 #include <set>
 #include <fstream>
 #include <string.h>
-//#include "soft_i2c.h"
 #include<vector>
 #include<cmath>
 #include<string>
@@ -70,15 +69,12 @@ bme280_data stream_sensor_data_forced_mode(struct bme280_dev *dev)
   int8_t rslt;
   uint8_t settings_sel;
   struct bme280_data comp_data;
-
   /* Recommended mode of operation: Indoor navigation */
   dev->settings.osr_h = BME280_OVERSAMPLING_1X;
   dev->settings.osr_p = BME280_OVERSAMPLING_16X;
   dev->settings.osr_t = BME280_OVERSAMPLING_2X;
   dev->settings.filter = BME280_FILTER_COEFF_16;
-
   settings_sel = BME280_OSR_PRESS_SEL | BME280_OSR_TEMP_SEL | BME280_OSR_HUM_SEL | BME280_FILTER_SEL;
-
   rslt = bme280_set_sensor_settings(settings_sel, dev);
   rslt = bme280_set_sensor_mode(BME280_FORCED_MODE, dev);
   /* Wait for the measurement to complete and print data @25Hz */
@@ -86,7 +82,6 @@ bme280_data stream_sensor_data_forced_mode(struct bme280_dev *dev)
   rslt = bme280_get_sensor_data(BME280_ALL, &comp_data, dev);
   return comp_data;
 }
-
 
 int main(int argc,char **argv)
 {
@@ -99,8 +94,8 @@ int main(int argc,char **argv)
     }
     if (ioctl(fd, I2C_SLAVE, 0x76) < 0) 
     {
-    printf("Failed to acquire bus access and/or talk to slave.\n");
-    exit(1);
+        printf("Failed to acquire bus access and/or talk to slave.\n");
+        exit(1);
     }
     dev.dev_id = BME280_I2C_ADDR_PRIM;
     dev.intf = BME280_I2C_INTF;
@@ -110,7 +105,6 @@ int main(int argc,char **argv)
     rslt = bme280_init(&dev);
     //Read ConfigFile
     ConfigReader conf("Slowcontrol","Database");
-    //conf.print();
     //Connect to Database
     Database database(conf.getParameters());
     database()->connect();
@@ -139,7 +133,6 @@ int main(int argc,char **argv)
         for(unsigned int i=0;i!=iter;++i)
         {
             bme280_data meas=stream_sensor_data_forced_mode(&dev);
-	    //print_sensor_data(&meas);
             pressure.push_back(meas.pressure/10000.0);
             temperature.push_back(meas.temperature/100.0);
             humidity.push_back(meas.humidity/1000.0);
@@ -176,8 +169,7 @@ int main(int argc,char **argv)
         std::string string4 = "VALUES (0,\""+tim.str()+"\","+to_stringN(mean_pressure)+","+to_stringN(std_pressure)+","+to_stringN(mean_temperature)+","+to_stringN(std_temperature)+","+to_stringN(mean_humidity)+","+to_stringN(std_humidity)+")";
         
         std::string com= string1 + string2 + string3 + string4;
-        //std::cout<<com<<std::endl;
-        //database()->execute(com);
+        database()->execute(com);
         std::cout<<tim.str()<<", Mean Pressure : "<<to_stringN(mean_pressure)<<" (Std : "<<to_stringN(std_pressure)<<"), Mean Temperature : "<<to_stringN(mean_temperature)<<" (Std : "<<to_stringN(std_temperature)<<"), Mean Humidity : "<<to_stringN(mean_humidity)<<" (Std : "<<to_stringN(std_humidity)<<")"<<std::endl;
         std::chrono::high_resolution_clock::time_point t2=std::chrono::high_resolution_clock::now();
         long rest=time*1000000-std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
